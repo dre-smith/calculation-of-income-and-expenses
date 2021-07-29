3f556b93b001734fe04d3def1d19a03d9c88991d
Andrey <steamprofile2020@gmail.com>
2021-07-29 08:25:49 +0300

    добавлено задание по уроку 1, попытка 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let money
+let income
+let addExpenses
+let deposit
+let mission
+let period
+
+alert('Вывести на экран в модальном окне (alert) сообщение с любым текстом')
+console.log('Вывести в консоль сообщение с любым текстом')
