a097a74be12fbca0086ab6babaff09fd0446c71e
Andrey <87332918+dre-smith@users.noreply.github.com>
2021-07-30 08:53:00 +0300

    Delete

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
-
-alert('Вывести на экран в модальном окне (alert) сообщение с любым текстом');
-console.log('Вывести в консоль сообщение с любым текстом');
