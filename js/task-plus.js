e8a90bf21d14ed6b7989b9913f93f39611623792
Andrey <steamprofile2020@gmail.com>
2021-08-02 22:28:39 +0300

    добавлено усложнённое задание 3 c исправлением

--- js/task-plus.js
+++ js/task-plus.js
@@ -1,3 +1,19 @@
+// УСЛОЖНЁННОЕ ЗАДАНИЕ 2
+
+let num = 266219,
+    result = true;
+
+num = num.toString().split('');
+
+for (let i = 0; i < num.length; i++) {
+    result *= num[i];
+}
+
+result = String(result ** 3).substring(0, 2);
+console.log(result);
+
+// УСЛОЖНЁННОЕ ЗАДАНИЕ 3
+
 let lang;
 
 function getLang1(value) {
